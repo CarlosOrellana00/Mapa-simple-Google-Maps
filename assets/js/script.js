636a33153794427717661a2103c1084d42c6183e
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-06-14 23:06:51 -0400

    2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 function iniciarMap(){
-  var coordenadas = {lat:, lng};
+  var coordenadas = {lat:-33.4374154 ,lng:-70.6512777};
   var map = new google.maps.Map(document.getElementById('map'),{
     zoom:10,
     center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
   });
 }
